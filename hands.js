060ea7940db8d25ff38d66455606f7addda64929
Daffa Shafwan <moch.chairullah@tiket.com>
2024-02-04 22:31:09 +0700

    checkpoint

--- hands.js
+++ hands.js
@@ -67,154 +67,38 @@ async function handleClick(event) {
 
   event.target.parentNode.appendChild(canvas);
   const cxt = canvas.getContext("2d");
-  for (const landmarks of handLandmarkerResult.landmarks) {
-    drawConnectors(cxt, landmarks, HAND_CONNECTIONS, {
-      color: "#00FF00",
-      lineWidth: 5
-    });
-    drawLandmarks(cxt, landmarks, { color: "#FF0000", lineWidth: 1 });
-  }
+  var twibbonImage = new Image();
+  twibbonImage.src = 'amin-twibbon.png';
+  twibbonImage.onload = function () {
+    cxt.drawImage(twibbonImage, 0, 0, canvas.width, canvas.height);
+    cxt.fillStyle = 'white';
+    cxt.font = '20px Arial';
+    cxt.fillText('Your Text Here', 10, 30);
+  };
   console.log(handLandmarkerResult.landmarks[0])
   //detectPose(handLandmarkerResult.landmarks[0])
   let wording = "gak niat bjir"
   let fingers = countFingers(handLandmarkerResult.landmarks[0])
 
-  if (fingers == 1){
+  if (fingers == 1) {
     wording = "Buzzer janji manies kh ?"
   }
 
-  if (fingers == 2){
+  if (fingers == 2) {
     wording = "Pendukung dinasti bjir"
   }
 
-  if (fingers == 3){
+  if (fingers == 3) {
     wording = "Skip wadas kocak"
   }
 
-  if (fingers == 4){
+  if (fingers == 4) {
     wording = "Lu temen gw"
   }
   alert(wording)
-}
-
-function detectPose(landmarks) {
-
-  let thumbsF = new Map();
-  let indexF = new Map();
-  let middleF = new Map();
-  let ringF = new Map();
-  let pinkyF = new Map();
-  let countLinear = 0;
-
-  for (const [index, value] of landmarks.entries()) {
-    if (index >= 1 && index <= 4) {
-      thumbsF.set(index.toString(), value)
-    }
-    if (index >= 5 && index <= 8) {
-      indexF.set(index.toString(), value)
-    }
-    if (index >= 9 && index <= 12) {
-      middleF.set(index.toString(), value)
-    }
-    if (index >= 13 && index <= 16) {
-      ringF.set(index.toString(), value)
-    }
-    if (index >= 17 && index <= 20) {
-      pinkyF.set(index.toString(), value)
-    }
-  }
-
-  if (checkFinger(thumbsF, "thumbsF")) {
-    console.log("thumbsF")
-    countLinear++;
-  }
-  if (checkFinger(indexF, "indexF")) {
-    console.log("indexF")
-    countLinear++;
-  }
-  if (checkFinger(middleF, "middleF")) {
-    console.log("middleF")
-    countLinear++;
-  }
-  if (checkFinger(ringF, "ringF")) {
-    console.log("ringF")
-    countLinear++;
-  }
-  if (checkFinger(pinkyF, "pinkyF")) {
-    console.log("pinkyF")
-    countLinear++;
-  }
-
-  let wording = "gak niat milih bjir"
-
-  if (countLinear == 1){
-    wording = "Buzzer janji manies kh ?"
-  }
-
-  if (countLinear == 2){
-    wording = "Pendukung dinasti bjir"
-  }
-
-  if (countLinear == 3){
-    wording = "Skip wadas kocak"
-  }
-
-  if (countLinear == 4){
-    wording = "Lu temen gw"
-  }
-  console.log(countLinear)
-  alert(wording)
-}
-
-function checkFinger(fingers, fingerName) {
-  let x1 = 0;
-  let y1 = 0;
-  let x2 = 0;
-  let y2 = 0;
-  let x3 = 0;
-  let y3 = 0;
-  let x4 = 0;
-  let y4 = 0;
-  for (const [index, value] of fingers.entries()) {
-    if (index == 1 || index == 5 || index == 9 || index == 13 || index == 17) {
-      x1 = value.x
-      y1 = value.y
-    }
-    if (index == 2 || index == 6 || index == 10 || index == 14 || index == 18) {
-      x2 = value.x
-      y2 = value.y
-    }
-    if (index == 3 || index == 7 || index == 11 || index == 15 || index == 19) {
-      x3 = value.x
-      y3 = value.y
-    }
-    if (index == 4 || index == 8 || index == 12 || index == 16 || index == 20) {
-      x4 = value.x
-      y4 = value.y
-    }
-  }
-
-  let isLinear = arePointsCollinear(x1, y1, x2, y2, x3, y3, x4, y4)
-
-  console.log(fingerName, isLinear)
-  return isLinear
-}
-
-function arePointsCollinear(x1, y1, x2, y2, x3, y3, x4, y4) {
-  const slopeAB = (y2 - y1) / (x2 - x1);
-  const slopeAC = (y3 - y1) / (x3 - x1);
-  const slopeAD = (y4 - y1) / (x4 - x1);
-
-  const tolerance = 3;
-  if (Math.abs(slopeAB - slopeAC) >= tolerance) {
-    return false;
-  }
-
-  return Math.abs(slopeAD - slopeAB) < tolerance;
 }
 
 function countFingers(handLandmarks) {
-  // Define landmarks for each finger
   const fingerLandmarks = [
     [4, 3, 2, 1],  // Thumb (using the tip of the thumb)
     [8, 7, 6, 5],  // Index finger
@@ -233,5 +117,5 @@ function countFingers(handLandmarks) {
     }
   }
 
-  return fingerCount-1
+  return fingerCount - 1
 }
